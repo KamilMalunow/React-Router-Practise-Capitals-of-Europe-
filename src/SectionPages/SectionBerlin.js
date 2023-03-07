5d026af610fa154db206a1a2475c8205237ad89f
Kamil Malunow <kamilmalunow@gmail.com>
2023-03-07 19:07:28 +0100

    fixed 404 error

--- src/SectionPages/SectionBerlin.js
+++ src/SectionPages/SectionBerlin.js
@@ -1,7 +1,8 @@
 const SectionBerlin = () => {
     return (
         <>
-            sectionberlin
+            <h1>Berlin the capital of Germany</h1>
+            <p>Berlin is the capital and largest city of Germany by both area and population. Its 3.7 million inhabitants make it the European Union's most populous city, according to population within city limits. One of Germany's sixteen constituent states, Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital. Berlin's urban area, which has a population of around 4.5 million, is the second most populous urban area in Germany after the Ruhr.The Berlin-Brandenburg capital region has around 6.2 million inhabitants and is Germany's third-largest metropolitan region after the Rhine-Ruhr and Rhine-Main regions.</p>
         </>
     )
 }
